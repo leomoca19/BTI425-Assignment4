9ae2452981ded2bfd75db5dfddc7f6536db9fe9e
Leonardo Caceres <leomoca19@gmail.com>
2025-02-18 00:25:56 -0500

    add signature

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+ * BTI425 – Assignment 4
+ * * I declare that this assignment is my own work in accordance with Seneca's
+ * Academic Integrity Policy:
+ *
+ * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+ *
+ * Name: Leonardo de la Mora Caceres Student ID: 152877205 Date: Feb-17-2025
+ ********************************************************************************/
+
 import Head from 'next/head'
 import Image from 'next/image'
 import { Geist, Geist_Mono } from 'next/font/google'
