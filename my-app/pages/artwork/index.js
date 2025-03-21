d08dccdc7da925fd967bd01c5469727ac3586cef
Leonardo Caceres <leomoca19@gmail.com>
2025-03-20 23:01:04 -0400

    use valid id data to filter results

--- my-app/pages/artwork/index.js
+++ my-app/pages/artwork/index.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import Error from 'next/error'
 import { Row, Col, Card, Pagination } from 'react-bootstrap'
 import ArtworkCard from '@/components/ArtworkCard'
+import validObjectIDList from '@/public/data/validObjectIDList.json'
 const PER_PAGE = 12
 
 export default function Artwork() {
@@ -18,6 +19,23 @@ export default function Artwork() {
     `https://collectionapi.metmuseum.org/public/collection/v1/search?${finalQuery}`
   )
 
+  useEffect(() => {
+    if (data) {
+      let results = []
+      let filteredValidIds = validObjectIDList.objectIDs.filter((x) =>
+        data.objectIDs?.includes(x)
+      )
+
+      for (let i = 0; i < filteredValidIds.length; i += PER_PAGE) {
+        const chunk = filteredValidIds.slice(i, i + PER_PAGE)
+        results.push(chunk)
+      }
+
+      setArtworkList(results)
+      setPage(1)
+    }
+  }, [data])
+
   if (error) return <Error statusCode={404} />
 
   function previousPage() {
@@ -26,20 +44,6 @@ export default function Artwork() {
   function nextPage() {
     if (page < artworkList.length) setPage(page + 1)
   }
-
-  useEffect(() => {
-    if (data) {
-      let results = []
-
-      for (let i = 0; i < data?.objectIDs?.length; i += PER_PAGE) {
-        const chunk = data?.objectIDs.slice(i, i + PER_PAGE)
-        results.push(chunk)
-      }
-
-      setArtworkList(results)
-      setPage(1)
-    }
-  }, [data])
 
   if (artworkList) {
     const nothingHere = (
